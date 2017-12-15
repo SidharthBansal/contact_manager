c453c14fb717c29d8c576fdd3c9e453ec0bd2484
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-15 20:54:24 +0530

    Added validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,17 @@ class Address < ApplicationRecord
 
   belongs_to :person
 
-  validates :postcode, length: { maximum: 8 }
+  validates :postcode, numericality: { greater_than_or_equal_to: 10000,
+                                       less_than_or_equal_to: 99999999,
+                                       only_integer: true }
 
+  validates :city, length: {maximum: 15 },
+                            format: {with: /\A[a-zA-Z\s]+\z/,
+                            message: "Only allows letters"}
+  validates :country, length: {maximum: 15 },
+                      format: {with: /\A[a-zA-Z\s]+\z/,
+                              message: "Only allows letters"}
+  validates :state, length: {maximum: 15 },
+                    format: {with: /\A[a-zA-Z\s]+\z/,
+                            message: "Only allows letters"}
 end
