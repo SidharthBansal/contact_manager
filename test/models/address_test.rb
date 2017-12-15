8943511b678f3e30ed81d100126bb573edeec7dc
Sidharth <bansal.sidharth2996@gmail.com>
2017-12-15 20:39:50 +0530

    Added Tests

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -15,11 +15,11 @@ class AddressTest < ActiveSupport::TestCase
   end
 
   test "postcode should be at least 4 chr long" do
-    assert true
+    @address.postcode = ("1" * 4).to_i
   end
 
   test "postcode shouldn't be longer that 8 char" do
-    @address.postcode = "1" * 9
+    @address.postcode = ("1" * 9).to_i
     assert_not @address.valid?
   end
 
@@ -38,6 +38,29 @@ class AddressTest < ActiveSupport::TestCase
     assert_not @address.valid?
   end
 
+  test "city names shouldnt contain numbers or symbols" do
+    bad_city_names = ["N3w Delhi", "N*w Delhi"]
+    bad_city_names.each do |name|
+      @address.city = name
+      assert_not @address.valid?
+    end
+  end
+
+  test "state names shouldn't contain numbers or symbols" do
+    bad_state_names = ["n3fak", "ast*", "fajk&agd"]
+    bad_state_names.each do |name|
+      @address.state = name
+      assert_not @address.valid?
+    end
+  end
+
+  test "country names shouldnt contain numbers or symbols" do
+    bad_country_names = ["!nda", "1ndia", "Un1ted States", "Un!ted States"]
+    bad_country_names.each do |name|
+      @address.country = name
+      assert_not @address.valid?
+    end
+  end
 
 
 end
